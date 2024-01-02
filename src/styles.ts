f5802548516834d183c84712b0562246cf1fd7a3
Douglas Eduardo <douglas.patusco@gmail.com>
2024-01-02 18:46:45 -0300

    construçcao do cabecalho

--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,6 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
-const cores = {
+export const cores = {
   branca: '#EEEEEE',
   preto: '#111111',
   cinza: '#333333',
@@ -12,11 +12,20 @@ export const GlobalCss = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: Roboto sans-serif;
+    font-family: Roboto, sans-serif;
+    list-style: none;
   }
 
   body {
     background-color: ${cores.preto};
     color: ${cores.branca};
+    padding-top: 40px;
+    margin-bottom: 80px;
   }
+`
+
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
 `
