14af8dc149c0afaa97dba7bb2ac16959e09c6c1f
Douglas Eduardo <douglas.patusco@gmail.com>
2024-01-03 18:29:13 -0300

    Componentizacao I

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,15 +1,25 @@
 import { Imagem, Titulo, Precos } from './styles'
 
 import bannerImg from '../../assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import Button from '../Button'
 
 const Banner = () => (
   <Imagem style={{ backgroundImage: `url(${bannerImg})` }}>
     <div className="container">
+      <Tag size="big">Destaque do dia</Tag>
       <Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulo>
       <Precos>
         De <span>R$ 250,00</span> <br />
         por apenas R$ 99,90
       </Precos>
+      <Button
+        type="link"
+        to="/produto"
+        title="Clique aqui para aproveitar essa oferta"
+      >
+        Aproveitar
+      </Button>
     </div>
   </Imagem>
 )
