680ddfb64f8fbcf037087afa8929c624d520d7ec
Douglas Eduardo <douglas.patusco@gmail.com>
2024-01-05 18:22:57 -0300

    Construindo as paginas

---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,106 @@
+import Game from '../../Models/Game'
+import Banner from '../../components/Banner'
+import ProductList from '../../components/ProductList'
+
+import resident from './../../assets/images/resident.png'
+import diablo from './../../assets/images/diablo.png'
+import zelda from './../../assets/images/zelda.png'
+import starWars from './../../assets/images/star_wars.png'
+import fifa23 from '../../assets/images/fifa-23.png'
+import streetFighter from '../../assets/images/street-fighter-6.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  },
+  {
+    id: 3,
+    category: 'Esporte',
+    description:
+      'EA SPORTS™ FIFA 23 traz o Jogo de Todo Mundo aos gramados com a tecnologia HyperMotion2...',
+    title: 'FIFA 23',
+    system: 'PS5',
+    infos: ['5%', 'R$ 290,00'],
+    image: fifa23
+  },
+  {
+    id: 4,
+    category: 'Esporte',
+    description:
+      'EA SPORTS™ FIFA 23 traz o Jogo de Todo Mundo aos gramados com a tecnologia HyperMotion2...',
+    title: 'FIFA 23',
+    system: 'PS5',
+    infos: ['10%', 'R$ 250,00'],
+    image: fifa23
+  }
+]
+
+const emBreve: Game[] = [
+  {
+    id: 5,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Diablo IV',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: diablo
+  },
+  {
+    id: 6,
+    category: 'Aventura',
+    description:
+      'Star Wars Jedi: Survivor é um próximo jogo de ação e aventura desenvolvido pela Respawn...',
+    title: 'Star Wars Jedi Survivor',
+    system: 'Windows',
+    infos: ['05/04'],
+    image: starWars
+  },
+  {
+    id: 7,
+    category: 'Luta',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Street Fighter 6',
+    system: 'Windows',
+    infos: ['04/05'],
+    image: streetFighter
+  },
+  {
+    id: 8,
+    category: 'RPG',
+    description:
+      'Uma aventura épica pela terra e pelos céus de Hyrule aguarda em The Legend of Zelda™...',
+    title: 'The Legend of Zelda - TOK',
+    system: 'Switch',
+    infos: ['04/05'],
+    image: zelda
+  }
+]
+
+const Home = () => (
+  <>
+    <Banner />
+    <ProductList games={promocoes} title="promoções" background="gray" />
+    <ProductList games={emBreve} title="Em breve" background="black" />
+  </>
+)
+
+export default Home
